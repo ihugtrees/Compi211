7d3794688bda5a3807b9c3581817b1805b71dc8e
CiTRuS93 <hadarradah@gmail.com>
2020-11-12 20:10:58 +0200

    fucking finished

--- reader.ml
+++ reader.ml
@@ -37,8 +37,7 @@ module Reader: sig
 end
 = struct
 (* let read_sexprs string = parser (string_to_list string);; *)
-let read_sexprs string = raise X_not_yet_implemented;;
-end;;
+
 let normalize_scheme_symbol str =
   let s = string_to_list str in
   if (andmap
@@ -55,17 +54,13 @@ let make_paired nt_left nt_right nt =
   let nt = pack nt (function(e, _) -> e) in
   nt;;
 
-let make_spaced nt = make_paired (star nt_whitespace) (star nt_whitespace) nt;;
+  
+
 let digit_nt = range '0' '9';;
 let nt_semi_colon = char ';';;
 let nt_end_of_line = char '\n';;
 let nt_end_of_file = pack nt_end_of_input (fun _ -> '\n');;
 
-let nt_line_comment =
-  let nt_end = disj nt_end_of_line nt_end_of_file in
-  let nt_comment = star (diff nt_any nt_end) in
-  let nt_whole_comment = caten (caten nt_semi_colon nt_comment) nt_end in
-  pack (make_spaced nt_whole_comment) (fun (_) -> Nil);;
 
 let bool_nt =
   let hash = char '#' in
@@ -76,6 +71,29 @@ let bool_nt =
   let nt = pack nt (fun (_,n)-> Bool ((lowercase_ascii n)='t')) in
   nt;;
 
+
+(*                             Chars                        *)
+
+
+let char_perfix_nt = word "#\\";;
+
+let named_char_nt =
+  disj_list
+  [pack (word_ci "nul") (fun _ -> '\000');
+   pack (word_ci "newline") (fun _ -> '\010');
+   pack (word_ci "return") (fun _ -> '\013');
+   pack (word_ci "tab") (fun _ -> '\009');
+   pack (word_ci "page") (fun _ -> '\012');
+   pack (word_ci "space") (fun _ -> '\032')];;
+
+
+let visible_simple_char_nt = const (fun c-> c > ' ');;
+
+let char_nt =
+  let any_char = (disj named_char_nt visible_simple_char_nt) in 
+  pack (caten char_perfix_nt any_char) (fun (_,c)->(c));;
+
+  
 
 (*                             Numberssssssss                        *)
 
@@ -153,12 +171,12 @@ test_string char_nt "#\\f";;
 
 let nt_string_meta_char =
   disj_list 
-  [pack (word "\\\\") (fun _ -> '\092');
-  pack (word "\\\"") (fun _ -> '\034');
-  pack (word "\\t") (fun _ -> '\009');
-  pack (word "\\f") (fun _ -> '\012');
-  pack (word "\\n") (fun _ -> '\010');
-  pack (word "\\r") (fun _ -> '\013');];;
+  [pack (word_ci "\\\\") (fun _ -> '\092');
+  pack (word_ci "\\\"") (fun _ -> '\034');
+  pack (word_ci "\\t") (fun _ -> '\009');
+  pack (word_ci "\\f") (fun _ -> '\012');
+  pack (word_ci "\\n") (fun _ -> '\010');
+  pack (word_ci "\\r") (fun _ -> '\013');];;
 
 let nt_string_literal_char =
   diff (diff nt_any (char '\"')) (char '\\');;
@@ -171,26 +189,25 @@ let nt_string =
   (fun ((_,chars),_)-> (list_to_string chars));;
 
 
-(*                             Chars                        *)
+(*                             Symbol                        *)
 
 
 let nt_symbol_char_no_dot =
   disj_list [char '!';  char '$';  char '^';  char '*';  char '-';  char '_';  char '=';
   char '+'; char '<';  char '>';  char '?';  char '/';  char ':';
   (range 'a' 'z');
-  (range 'A' 'Z');
+  pack (range 'A' 'Z') (fun ch -> lowercase_ascii ch);
   (range '0' '9')];;
 
-let nt_dot = char '.';;
-
-let nt_symbol_char = disj nt_symbol_char_no_dot nt_dot;;
+let nt_symbol_char = 
+  let nt_dot = char '.' in
+  disj nt_symbol_char_no_dot nt_dot;;
 
 let nt_symbol =
   let symb = pack (caten nt_symbol_char (plus nt_symbol_char)) (fun(s,lst)->s::lst) in
   let doted = pack symb (fun (chars)->list_to_string chars) in
   let not_doted = pack (plus nt_symbol_char_no_dot) (fun (chars)->list_to_string chars) in
   disj doted not_doted;;
-
 
 let char_obj = 
   pack char_nt (fun (c)->Char c);;
@@ -201,13 +218,20 @@ let symbol_obj =
 let string_obj = 
   pack nt_string (fun(str)->String(str));;
 
-
 (*                             List                        *)
 
-let rec parse_sexpr str=
-  (make_spaced 
+let nt_whitespaces = pack (plus nt_whitespace) (fun (a)->' ');;
+
+let nt_line_comment =
+  let nt_end = disj nt_end_of_line nt_end_of_file in
+  let nt_comment = star (diff nt_any nt_end) in
+  let nt_whole_comment = (caten (caten nt_semi_colon nt_comment) nt_end) in 
+  pack nt_whole_comment (fun (a)->' ');;
+
+let rec parse_sexpr str =
+  (make_paired (star comment_and_space) (star comment_and_space) 
     (disj_list 
-          [ nt_line_comment;
+          [ 
             bool_nt;
             number_nt;
             char_obj;
@@ -222,11 +246,12 @@ let rec parse_sexpr str=
             ]
       )) str
 
-and parse_list str= 
+and parse_list str = 
   (pack (caten (char '(') (caten (star parse_sexpr) (char ')')))
   (fun (left,(lst,right))-> match lst with
           | []-> Nil
           | _-> (List.fold_right (fun a b -> Pair (a,b)) lst Nil))) str
+          
 and parse_dot_list str = 
   let start = caten (char '(') (caten (plus parse_sexpr) (char '.')) in
   let pend = caten parse_sexpr (char ')') in
@@ -236,34 +261,40 @@ and parse_dot_list str =
     (List.fold_right (fun a b -> Pair (a,b)) s e)
   ))
   str
+  
 and parse_quote str =
   (pack (caten (char '\'') parse_sexpr)
   (fun (q,s)->Pair(Symbol("quote"),s)))
   str
+
 and parse_qquote str =
   (pack (caten (char '`') parse_sexpr)
   (fun (q,s)->Pair(Symbol("quasiquote"),s)))
   str
+  
 and parse_unquote_splice str =
   (pack (caten (word ",@") parse_sexpr)
   (fun (q,s)->Pair(Symbol("unquote-splicing"),s)))
   str
+  
 and parse_unquote str =
   (pack (caten (char ',') parse_sexpr)
   (fun (q,s)->Pair(Symbol("unquote"),s)))
   str
-  ;;
+  
+and nt_inline_comment str =
+  let perfix = word "#;" in
+  let comment_sepr = caten perfix parse_sexpr in 
+  (pack comment_sepr (fun (_) -> ' ')) str
+
+and comment_and_space str =
+     disj_list[nt_whitespaces; nt_line_comment; nt_inline_comment] str;;
 
 let parser s =
   let (res, rest) = (star parse_sexpr) s in
   res;;
 
-(*
-test_string (star string_obj) "\"sasdasdas\"";;
-test_string (star parse_sexpr) "  ;ssdf\n  \"sdad\"(x+2 +3.2 #\\e)";;
- test_string (make_spaced number_nt) "   2.2E5   ";;
-test_string nt_line_comment "  ; 1234  ";; 
-*)
-
-
- (* struct Reader *)
+parser (string_to_list "(+ 1 2) 1 \"only one\"");;
+let read_sexprs string = parser (string_to_list string);;
+end;;
+ (* struct Reader *) 
