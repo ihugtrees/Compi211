a2f3d1fb944196aa513f5f4fe4d6d87fcb485245
ihugtrees <igoralikin@gmail.com>
2021-01-05 20:49:02 +0200

    before generate

--- code-gen.ml
+++ code-gen.ml
@@ -34,11 +34,15 @@ end;;
 
 module Code_Gen : CODE_GEN = struct
 
-let rec find_sexpr_offset sexp const_tbl = 
-    match const_tbl with 
-    | entry::rest -> let (cur_sexpr, (offset, _)) = entry in
-        if (cur_sexpr = sexp) then (string_of_int offset) else find_sexpr_offset sexp rest
-    | [] -> "---------WARNING NO SUCH CONST DEFINED-------------";;
+let const_eq e1 e2 =
+    match e1, e2 with
+    | Void , Void -> true
+    | Sexpr(sexpr1) , Sexpr(sexpr2) -> sexpr_eq sexpr1 sexpr2
+    | _ , _ -> false
+    
+let get_constant_offset sexp consts =
+let (_,(off,_)) = List.find (fun ((sexpr, (offset, str))) -> const_eq sexp sexpr) consts in
+    (string_of_int off);;
 
 let rec expand_sexpr expr =
   match expr with
@@ -51,27 +55,27 @@ let expand_sexpr_list sexpr_list =
 
 let sexpr_to_const_entry sexpr const_tbl curr_off =
       match sexpr with
-      | Void -> ((Void, (curr_off, "MAKE_VOID\n")), curr_off + 1)
-      | Sexpr(Nil) -> ((Sexpr(Nil), (curr_off, "MAKE_NIL\n")), curr_off + 1)
-      | Sexpr(Char(c)) -> ((Sexpr(Char(c)), (curr_off, "MAKE_LITERAL_CHAR(" ^ (String.make 1 c) ^ ")\n")), curr_off + 2)
-      | Sexpr(Bool(false)) -> ((Sexpr(Bool(false)), (curr_off, "MAKE_LITERAL_BOOL(0)\n") ), curr_off + 2)
-      | Sexpr(Bool(true)) -> ((Sexpr(Bool(true)), (curr_off, "MAKE_LITERAL_BOOL(1)\n") ), curr_off + 2)
-      | Sexpr(Number(Fraction(num, denum))) -> ((Sexpr(Number(Fraction(num, denum))), (curr_off , "MAKE_LITERAL_RATIONAL(" ^ (string_of_int num) ^ "," ^ (string_of_int denum) ^ ")\n")), curr_off + 17)
-      | Sexpr(Number(Float(num))) -> ((Sexpr(Number(Float(num))), (curr_off, "MAKE_LITERAL_FLOAT(" ^ (string_of_float num) ^ ")\n")), curr_off + 9)
-      | Sexpr(String(str)) -> ((Sexpr(String(str)), (curr_off, "MAKE_LITERAL_STRING " ^ str ^"\n")), curr_off + 9 + (String.length str))
-      | Sexpr(Symbol(sym)) -> ((Sexpr(Symbol(sym)), (curr_off, "MAKE_LITERAL_SYMBOL(const_tbl+" ^ (find_sexpr_offset (Sexpr(String(sym))) const_tbl) ^ ")\n")), curr_off + 9)
-      | Sexpr(Pair(car, cdr)) -> ((Sexpr(Pair(car,cdr)), (curr_off, "MAKE_LITERAL_PAIR(const_tbl+" ^ (find_sexpr_offset (Sexpr(car)) const_tbl) ^ ",const_tbl+" ^ (find_sexpr_offset (Sexpr(cdr)) const_tbl) ^")\n")), curr_off + 17)
+      | Void -> ((Void, (curr_off, "MAKE_VOID")), curr_off + 1)
+      | Sexpr(Nil) -> ((Sexpr(Nil), (curr_off, "MAKE_NIL")), curr_off + 1)
+      | Sexpr(Char(c)) -> ((Sexpr(Char(c)), (curr_off, "MAKE_LITERAL_CHAR(" ^ (String.make 1 c) ^ ")")), curr_off + 2)
+      | Sexpr(Bool(false)) -> ((Sexpr(Bool(false)), (curr_off, "MAKE_LITERAL_BOOL(0)") ), curr_off + 2)
+      | Sexpr(Bool(true)) -> ((Sexpr(Bool(true)), (curr_off, "MAKE_LITERAL_BOOL(1)") ), curr_off + 2)
+      | Sexpr(Number(Fraction(num, denum))) -> ((Sexpr(Number(Fraction(num, denum))), (curr_off , "MAKE_LITERAL_RATIONAL(" ^ (string_of_int num) ^ "," ^ (string_of_int denum) ^ ")")), curr_off + 17)
+      | Sexpr(Number(Float(num))) -> ((Sexpr(Number(Float(num))), (curr_off, "MAKE_LITERAL_FLOAT(" ^ (string_of_float num) ^ ")")), curr_off + 9)
+      | Sexpr(String(str)) -> ((Sexpr(String(str)), (curr_off, "MAKE_LITERAL_STRING " ^ str ^"")), curr_off + 9 + (String.length str))
+      | Sexpr(Symbol(sym)) -> ((Sexpr(Symbol(sym)), (curr_off, "MAKE_LITERAL_SYMBOL(const_tbl+" ^ (get_constant_offset (Sexpr(String(sym))) const_tbl) ^ ")")), curr_off + 9)
+      | Sexpr(Pair(car, cdr)) -> ((Sexpr(Pair(car,cdr)), (curr_off, "MAKE_LITERAL_PAIR(const_tbl+" ^ (get_constant_offset (Sexpr(car)) const_tbl) ^ ",const_tbl+" ^ (get_constant_offset (Sexpr(cdr)) const_tbl) ^")")), curr_off + 17)
 
 let rec build_const_tbl sexprs const_tbl curr_off =
   match sexprs with
-  | sexpr::rest -> let (entry, new_of) = (sexpr_to_const_entry sexpr const_tbl curr_off) in
-                      build_const_tbl rest (List.append const_tbl [entry]) new_of
-  | [] -> const_tbl
+  | sexpr::rest -> let (entry, new_off) = (sexpr_to_const_entry sexpr const_tbl curr_off) in
+                      build_const_tbl rest (List.append const_tbl [entry]) new_off
+  | [] -> const_tbl;;
 
 let rec remove_dup lst  =
   match lst with
   |h::t ->if List.mem h t then (remove_dup t) else h :: (remove_dup t)
-  |[] -> []
+  |[] -> [];;
 
 let rec create_free_vars_tuples vars index =
   match vars with
@@ -95,10 +99,17 @@ let rec find_free_vars ast =
         | Def'((VarFree(str)) , e) -> [str] @ (find_free_vars e)
         | Var'(VarFree(str)) -> [str]
         | _ -> [];;
-
+    
 let rec find_const ast =
   match ast with
-        | If' (test , dit , dif) -> (find_const test)@ (find_const dit) @ (find_const dif)
+        | Const'(Void) -> []
+        | Var'(VarFree(str)) -> []
+        | Const'(Sexpr(Symbol(str))) -> [(Sexpr(String(str))) ; (Sexpr(Symbol(str)))]
+        | Const'(Sexpr(String(str))) -> [(Sexpr(String(str)))]
+        | Const'(Sexpr(Pair(car,cdr))) -> (find_const (Const'(Sexpr(car)))) @ (find_const (Const'(Sexpr(cdr)))) @ [(Sexpr(Pair(car,cdr)))]
+        | Const'(Sexpr(Number(n))) ->[Sexpr(Number(n))]
+        | Const'(Sexpr(Char(n))) ->[Sexpr(Char(n))]
+        | If' (test , dit , dif) -> (find_const test) @ (find_const dit) @ (find_const dif)
         | Seq' (expr_list) -> List.fold_left (fun (acc) -> (fun (curr) -> acc @ curr)) [] (List.map find_const expr_list)
         | Set'(v, expr) -> (find_const expr)
         | Or' (expr_list) -> List.fold_left (fun (acc) -> (fun (curr) -> acc @ curr)) [] (List.map find_const expr_list)
@@ -109,28 +120,29 @@ let rec find_const ast =
         | BoxSet'(VarFree(str) , e) -> (find_const e)
         | BoxSet'(var , e) -> find_const e
         | Def'((VarFree(str)) , e) -> (find_const e)
-        | Var'(VarFree(str)) ->[]
-        | Const'(Void) -> []
-        | Const'(Sexpr(Symbol(str))) -> [(Sexpr(String(str))) ; (Sexpr(Symbol(str)))]
-        | Const'(Sexpr(String(str))) -> [(Sexpr(String(str)))]
-        | Const'(Sexpr(Pair(car,cdr))) -> (find_const (Const'(Sexpr(car)))) @ (find_const (Const'(Sexpr(cdr)))) @ [(Sexpr(Pair(car,cdr)))]
-        | Const'(Sexpr(Number(n))) ->[Sexpr(Number(n))]
         | _ -> [];;
 
-  let make_consts_tbl asts =
-    let consts = (List.rev (remove_dup (List.rev (List.flatten (List.map find_const asts))))) in
-    (* let sorted_sexprs_list = expand_sexpr_list consts  in *)
-    let sorted_sexprs_list = List.append [Void; Sexpr(Nil) ; Sexpr(Bool(false)) ; Sexpr(Bool(true))] consts in
-    let sorted_sexprs_set = remove_dup sorted_sexprs_list in
-    build_const_tbl sorted_sexprs_set [] 0 ;;
+let rec asm_from_expr consts fvars e depth =
+  match e with
+  | Const'(expr) -> let offset = get_constant_offset expr consts in
+  "mov rax, const_tbl+" ^ offset ;;
 
-  let make_fvars_tbl asts = (create_free_vars_tuples (remove_dup (List.flatten (List.map find_free_vars asts))) 0);;
+let make_consts_tbl asts =
+  let consts = (List.rev (remove_dup (List.rev (List.flatten (List.map find_const asts))))) in
+  (* let sorted_sexprs_list = expand_sexpr_list consts  in *)
+  let sorted_sexprs_list = List.append [Void; Sexpr(Nil) ; Sexpr(Bool(false)) ; Sexpr(Bool(true))] consts in
+  let sorted_sexprs_set = remove_dup sorted_sexprs_list in
+  build_const_tbl sorted_sexprs_set [] 0 ;;
 
-  let generate consts fvars e = raise X_not_yet_implemented;;
+let make_fvars_tbl asts = (create_free_vars_tuples (remove_dup (List.flatten (List.map find_free_vars asts))) 0);;
 
- 
+let generate consts fvars e =  
+
+ asm_from_expr consts fvars e 0;;
+
+
 end;;
-(* 
+(*
 let test_collect_sexp str =
   collect_sexp (Semantics.run_semantics (List.hd (Tag_Parser.tag_parse_expressions (Reader.read_sexprs str))));;
 
@@ -158,7 +170,16 @@ let test_make_fvars_table str =
 
 let ast =  List.map Semantics.run_semantics
                            (Tag_Parser.tag_parse_expressions
-                              (Reader.read_sexprs "(list\"ab\" '(1 2) 'c 'ab)"));;
+                              (Reader.read_sexprs "(define str (make-string 5 #\\space))
+
+(string-set! str 0 #\\t)
+(string-set! str 1 #\\l)
+(string-set! str 3 #\\n)
+(string-set! str 4 #\\newline)
+
+str
+
+"));;
 Code_Gen.make_consts_tbl ast;;
 (* Code_Gen.make_consts_tbl [
  Const' (Sexpr
